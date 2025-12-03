fa04f0dc0c83e15ac169cfafb8aaf085b2b2167d
Ritiksharmaji <ritiksharma555598@gmail.com>
2025-12-03 17:26:31 +0530

    adding render url for backend project

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-export const API_BASE_URL = "http://localhost:5454";
+// export const API_BASE_URL = "http://localhost:5454";
+export const API_BASE_URL = "https://shopping-backend-app-q3af.onrender.com";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
